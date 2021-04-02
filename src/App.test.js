c2445bdb71246a1728b67345c6aea6a9dac49cb3
lawcia <lawrencianjume@gmail.com>
2021-04-02 14:07:38 +0100

    add form with input and name

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name label', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameLabel = screen.getByText(/Name/i);
+  expect(nameLabel).toBeInTheDocument();
 });
