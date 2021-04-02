ebc1f88b186ad35bf7fb29cb6856c89926f08910
Joanna Czerwinska <joczerw@gmail.com>
2021-04-02 14:39:16 +0100

    Saving the input data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import React, {createContext, useState} from "react";
 
 function App() {
+const [input, setInput] = useState("");
+
+const onSubmitForm = (event) => {
+  event.preventDefault();
+  console.log(input);
+}
+
+const onInputChange = (event) => {
+  setInput(event.target.value);
+  console.log(input);
+}
+
   return (
     <div className="App">
-     <form>
-       <label for="name">Name</label>
-       <input type="text" id="name" />
-       <input type="submit" value="Enter" />
+     <form onSubmit={onSubmitForm}>
+       <label htmlFor="name">Name</label>
+       <input type="text" id="name" onChange={onInputChange}/>
+       <input type="submit" value="Enter"/>
      </form>
     </div>
   );
