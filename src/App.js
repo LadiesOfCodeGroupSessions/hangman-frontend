f19cf3341be3ae8965dfaa934454372e9930794e
lawcia <lawrencianjume@gmail.com>
2021-04-18 12:34:59 +0100

    add axios and lint
    - axios
    - lint

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import './App.css';
-import React, {createContext, useState} from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
-const onSubmitForm = (event) => {
-  event.preventDefault();
-  takeName();
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    takeName();
+  };
 
-}
+  const takeName = () => {
+    //need to change the backend for {name:input}
+    axios
+      .post('http://localhost:8080/name', { name: input })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('error in takeName function', error);
+      });
+  };
 
-const takeName = () => {
-   //need to change the backend for {name:input}
-  axios.post("http://localhost:8080/name", {name:input}).then(response => {
-    console.log(response);
-  }).catch(error => {
-    console.log("error in takeName function", error);
-  });
-}
-
-const onInputChange = (event) => {
-  setInput(event.target.value);
-  console.log(input);
-}
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+    console.log(input);
+  };
 
   return (
     <div className="App">
-     <form onSubmit={onSubmitForm}>
-       <label htmlFor="name">Name</label>
-       <input type="text" id="name" onChange={onInputChange}/>
-       <input type="submit" value="Enter"/>
-     </form>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" onChange={onInputChange} />
+        <input type="submit" value="Enter" />
+      </form>
     </div>
   );
 }
