ba27810959debc0597b46113fefb86ef863945d5
lawcia <lawrencianjume@gmail.com>
2021-04-18 13:58:15 +0100

    Update App.js
    save user data local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [input, setInput] = useState("");
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const userData = localStorage.getItem("hangman");
+    if (userData) {
+      setUser(JSON.parse(userData));
+    }
+  }, []);
 
   const onSubmitForm = (event) => {
     event.preventDefault();
@@ -11,11 +19,11 @@ function App() {
   };
 
   const takeName = () => {
-    //need to change the backend for {name:input}
     axios
-      .post("http://localhost:8080/name", { name: input })
+      .post("http://localhost:8080/api/v1/player", { name: input })
       .then((response) => {
-        console.log(response);
+        localStorage.setItem("hangman", JSON.stringify(response.data));
+        setUser(response.data);
       })
       .catch((error) => {
         console.log("error in takeName function", error);
@@ -24,16 +32,21 @@ function App() {
 
   const onInputChange = (event) => {
     setInput(event.target.value);
-    console.log(input);
   };
 
   return (
     <div className="App">
-      <form onSubmit={onSubmitForm}>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" onChange={onInputChange} />
-        <input type="submit" value="Enter" />
-      </form>
+      {user ? (
+        <div>
+          <h1>{user.name}</h1>
+        </div>
+      ) : (
+        <form onSubmit={onSubmitForm}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" onChange={onInputChange} />
+          <input type="submit" value="Enter" />
+        </form>
+      )}
     </div>
   );
 }
