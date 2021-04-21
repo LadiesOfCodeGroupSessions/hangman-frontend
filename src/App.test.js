c047452f983ccea8fb0666c613bce127e9deaaf2
Joanna Czerwinska <joczerw@gmail.com>
2021-04-21 19:30:22 +0100

    Modified a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,22 @@
 import React from "react";
 import App from "./App";
 import { shallow, mount } from "enzyme";
+import { createShallow, createMount } from '@material-ui/core/test-utils';
 
 describe('App', () => {
 
   it('Should have form', () => {
     const wrapper = shallow(<App 
       />);
+    console.log(wrapper.debug());
     expect(wrapper.find('form').length).toEqual(1);
   });
+
+  // it('Should have a button that works', () => {
+  //   const wrapper = createShallow(<App 
+  //     />);
+  //   console.log(wrapper.debug());
+  //   expect(wrapper.find()).toEqual(1);
+  // });
 
 });
