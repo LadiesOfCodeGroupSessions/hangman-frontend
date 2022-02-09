5b9e1060a52e866b52e323600ce77e136b35af1b
lawcia <lawrencianjume@gmail.com>
2022-02-09 18:14:35 +0000

    Past test for populating letter in position

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,12 +1,29 @@
 import "./Word.css";
 
-function Word(props){
-    const secretWordLength = new Array(props.secretWordLength);
-    const secretWordArray = secretWordLength.fill("_");
-    
-    return <div className= "word"> {secretWordArray.map((lines, index) => ( 
-        <span aria-label="letter" key={index}>{lines}</span> ))}
-        </div>
+function Word(props) {
+  const secretWordLength = new Array(props.secretWordLength);
+  const secretWordArray = secretWordLength.fill("_");
+  populateLetterInPosition();
+
+  function populateLetterInPosition() {
+    props.correctLetters?.forEach((correctLetter) => {
+      correctLetter.position.forEach((position) => {
+        secretWordArray[position] = correctLetter.letter;
+      });
+    });
+  }
+
+  return (
+    <ul className="word">
+      {secretWordArray.map((lines, index) => {
+        return (
+          <li aria-label="letter" key={index}>
+            {lines}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default Word;
--- src/components/Word.test.js
+++ src/components/Word.test.js
@@ -1,29 +1,25 @@
-import {render, screen} from "@testing-library/react";
-import Word from './Word';
+import { render, screen } from "@testing-library/react";
+import Word from "./Word";
 
+describe("Word", () => {
+  it("Should render 5 underscores if secret word length is 5", () => {
+    render(<Word secretWordLength={5} />);
+    screen.debug();
+    const span = screen.queryAllByText("_");
+    expect(span.length).toBe(5);
+  });
 
-describe('Word', () => {
-    it('Should render 5 underscores if secret word length is 5', () => {
-        render(<Word secretWordLength={5} />)
-        screen.debug()
-        const span = screen.queryAllByText('_')
-        expect(span.length).toBe(5);
-    })
+  it("Should render correctly guessed letter in the correct position", () => {
+    const correctLetters = [
+      {
+        letter: "A",
+        position: [1],
+      },
+    ];
 
-    it ('Should render correctly guessed letter in the correct position', () => {
-        const correctLetters = [
-            {
-                "letter": "A",
-                "position": [
-                    1
-                ]
-            }
-        ]
-
-        // TODO FINISH THIS TEST
-        // https://testing-library.com/docs/dom-testing-library/api-accessibility
-        render(<Word secretWordLength={5} correctLetters={correctLetters} />)
-        screen.debug()
-        const guess = screen.queryAllByRole('paragraph')
-    })
-})
+    render(<Word secretWordLength={5} correctLetters={correctLetters} />);
+    screen.debug();
+    const letterElements = screen.queryAllByRole("listitem");
+    expect(letterElements[1].textContent).toBe("A");
+  });
+});
